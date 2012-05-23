826f1e7598dfa0ff45a80d899e0eab4a2294cf86
Andy Davies <andy.davies@gmail.com>
2012-05-23 14:29:01 +0100

    initial commit

---
+++ src/js/application.js
@@ -0,0 +1,16 @@
+define([
+		'views/main-view'
+	],
+	function(MainView) {
+		"use strict";
+		
+		return {
+			initialize : function() {
+
+				var app = new MainView({
+					el : document.getElementById('content')
+				});
+			}
+		};
+	}
+);
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+(function() {
+	"use strict";
+	
+	var libsPath = '../../libs/js/'; //libsPath relative to 'basePath'
+	
+	require.config({
+		paths : {
+			jquery : libsPath + 'jquery-1.7.1.min',		//return jQuery module but also keeps it in the global scope
+			jqueryLoader : libsPath + 'jquery',		//loads jQuery module and removes it from the global scope
+			underscore : libsPath + 'underscore-min',
+			backbone : libsPath + 'backbone-min'
+		},
+		baseUrl : 'src/js'
+	});
+	
+	require([
+			'application'
+		],
+		function(App) {
+			App.initialize();
+		}
+	);
+})();
